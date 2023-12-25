0b117eba5f6194605e4cad1c70dd989a99143c79
Tatyana_Ospechceva <t.ospechceva@mail.ru>
2023-12-25 19:50:36 +0300

    main--

--- app/settings.py
+++ app/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import psycopg2
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,9 +125,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_FINDERS = (
+'django.contrib.staticfiles.finders.FileSystemFinder',
+'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+ )
--- main/views.py
+++ main/views.py
@@ -9,11 +9,17 @@ def index(request):
     context = {
         'title': 'OSPECHEV - Главная',
         'content': "Магазин мужской обуви OSPECHEV",
+        'name': "OSPECHEV",
     }
 
     return render(request, 'main/index.html', context)
 
 
 def about(request):
+    context = {
+        'title': 'OSPECHEV - О нас',
+        'content': "О нас",
+        'text_on_page': "Текст о том почему этот магазин такой классный, и какой хороший товар."
+    }
 
-    return HttpResponse('Apout page')
+    return render(request, 'main/about.html', context)
